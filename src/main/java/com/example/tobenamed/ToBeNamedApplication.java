a2b1b2b5921badfcda9ed1379632337458941d00
jan <jan.czapiewski@smartbear.com>
2025-01-09 20:18:55 +0100

    init

---
+++ src/main/java/com/example/tobenamed/ToBeNamedApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobenamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToBeNamedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToBeNamedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tobenamed/ToBeNamedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobenamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToBeNamedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+//nkjn
+}
